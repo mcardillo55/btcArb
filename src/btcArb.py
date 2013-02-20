eee38873eee897e9763cc85a3dac96f8b7106079
Mike Cardillo <mcardillo55@gmail.com>
2013-02-20 00:58:40 -0800

    separated/cleaned up code a little

--- src/btcArb.py
+++ src/btcArb.py
@@ -2,10 +2,17 @@
 import json
 import websocket
 
-mtgox = websocket.create_connection("ws://websocket.mtgox.com:80")
-ticker_cmd = "{'op':'mtgox.subscribe', 'type':'ticker'}"
-mtgox.send(ticker_cmd)
-mtgox.recv()
-while 1:
-    ticker = json.loads(mtgox.recv())["ticker"]
+def connect_mtgox():
+    sock = websocket.create_connection("ws://websocket.mtgox.com:80")
+    ticker_cmd = "{'op':'mtgox.subscribe', 'type':'ticker'}"
+    sock.send(ticker_cmd)
+    sock.recv()
+    return sock 
+    
+def get_mtgoxtick(sock):
+    ticker = json.loads(sock.recv())["ticker"]
     print "Last: %s Bid: %s Ask: %s" % (ticker['last']['value'], ticker['buy']['value'], ticker['sell']['value']) 
+    
+mtgox = connect_mtgox()
+while 1:
+    get_mtgoxtick(mtgox)
